0e2111830136514fe5b684d103e79824c236b530
FarazAfridi <farazafridi5159@gmail.com>
2020-12-19 00:16:35 +0500

    shopping-basket-completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { store } from "./store/index"
+import { Provider } from "react-redux";
+import Basket from './components/basket';
+import Product from './components/product';
+import { Container, Box } from "@material-ui/core"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <Container maxWidth="md">
+        <Box mt={5} mb={5}>
+          <Product />
+        </Box>
+        <Box mt={5} mb={5}>
+          <Basket />
+        </Box>
+      </Container>
+    </Provider>
+  )
 }
 
 export default App;
